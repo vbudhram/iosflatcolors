17aca129886a8b7292e545053e3c8b8d071dd138
vbudhram <vbudhram@gmail.com>
2014-05-21 21:03:01 -0400

    Added helper class
    Helper class to create flat UIColors

---
+++ BCFlatColor.m
@@ -0,0 +1,96 @@
+//
+//  BCFlatColor.m
+//  Memeizer
+//
+//  Created by Vijay Budhram on 5/21/14.
+//  Copyright (c) 2014 Baby Carrot Productions. All rights reserved.
+//
+
+#import "BCFlatColor.h"
+
+#define rgb(r, g, b)  [UIColor colorWithRed:((r) / 255.0) green:((g) / 255.0) blue:((b) / 255.0) alpha:1.0]
+
+@implementation BCFlatColor
+
++(UIColor *) turquoise
+{
+    return rgb(26, 188, 156);
+}
++(UIColor *) emerald
+{
+    return rgb(46, 204, 113);
+}
++(UIColor *) peterriver
+{
+    return rgb(52, 152, 219);
+}
++(UIColor *) amethyst
+{
+    return rgb(155, 89, 182);
+}
++(UIColor *) wetasphalt
+{
+    return rgb(52, 73, 94);
+}
++(UIColor *) greensea
+{
+    return rgb(22, 160, 133);
+}
++(UIColor *) nephritis
+{
+    return rgb(39, 174, 96);
+}
++(UIColor *) belizehole
+{
+    return rgb(41, 128, 185);
+}
++(UIColor *) wisteria
+{
+    return rgb(142, 68, 173);
+}
++(UIColor *) midnightblue
+{
+    return rgb(44, 62, 80);
+}
++(UIColor *) sunflower
+{
+    return rgb(241, 196, 15);
+}
++(UIColor *) carrot
+{
+    return rgb(230, 126, 34);
+}
++(UIColor *) alizarin
+{
+    return rgb(231, 76, 60);
+}
++(UIColor *) clouds
+{
+    return rgb(236, 240, 241);
+}
++(UIColor *) concrete
+{
+    return rgb(149, 165, 166);
+}
++(UIColor *) orange
+{
+    return rgb(243, 156, 18);
+}
++(UIColor *) pumpkin
+{
+    return rgb(211, 84, 0);
+}
++(UIColor *) pomegranate
+{
+    return rgb(192, 57, 43);
+}
++(UIColor *) silver
+{
+    return rgb(189, 195, 199);
+}
++(UIColor *) asbestos
+{
+    return rgb(127, 140, 141);
+}
+
+@end
